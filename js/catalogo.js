bc83bc83381da5b998a8628a82f59f91dfb33210
TwoHundredFaces <emersonsopares141@gmail.com>
2023-05-23 11:19:22 -0300

    outras coisas

---
+++ js/catalogo.js
@@ -0,0 +1,44 @@
+let modeloJson = [
+    {
+      id: 1,
+      name: 'Me chame pelo seu nome',
+      img: 'imgs/call.jpg',
+      description: 'O jovem Elio está enfrentando outro verão preguiçoso na casa de seus pais na bela e lânguida paisagem italiana. Mas tudo muda com a chegada de Oliver, um acadêmico que veio ajudar a pesquisa de seu pai.',
+      link: 'https://youtu.be/7yCwv8FjidU',
+    },
+    {
+      id: 2,
+      name: 'Given',
+      img: 'imgs/given.jpg',
+      description: 'A história segue um grupo de quatro alunos de uma banda de rock amadora e as relações românticas que se formam entre eles: entre o guitarrista Ritsuka Uenoyama e o vocalista Mafuyu Satō (e seu cachorro, Kedama Satō), e entre o baixista Haruki Nakayama e o baterista Akihiko Kaji.',
+      link: 'https://youtu.be/F1fRlQliZMI',
+    },
+    {
+      id: 3,
+      name: 'Classmates',
+      img: 'imgs/classmates.jpg',
+      description: 'Conta a história de Kusakabe e Sajou, dois alunos que nunca se falaram antes, mas curiosamente passaram a se encontrar para treinar canto para uma apresentação na escola.',
+      link: 'https://youtu.be/R8KZ9WOTU78',
+    },
+    {
+      id: 4,
+      name: 'Close',
+      img: 'imgs/close.jpg',
+      description: 'Léo e Rémi são dois garotos de treze anos que são melhores amigos. Eles passam as férias de verão juntos, compartilhando momentos muito próximos e inocentes. No entanto, na volta às aulas, os colegas começam a tecer comentários intrusivos sobre sua proximidade.',
+      link: 'https://youtu.be/dlNWImeXzsQ',
+    },
+    {
+      id: 5,
+      name: 'Hoje eu quero voltar sozinho',
+      img: 'imgs/hoje.jpg',
+      description: 'Leonardo, um adolescente cego, tenta lidar com a mãe superprotetora ao mesmo tempo em que busca sua independência. Quando Gabriel chega em seu colégio, novos sentimentos começam a surgir em Leonardo, fazendo com que ele descubra mais sobre si mesmo e sua sexualidade.',
+      link: 'https://youtu.be/lpHKXyko358',
+    },
+    {
+      id: 6,
+      name: 'Yes, no or maybe',
+      img: 'imgs/yesnomaybe.jpg',
+      description: 'A série é estrelada por Kei Kunieda, um jovem e popular apresentador de televisão que mantém sua fachada de perfeição diante das câmeras, mas que em particular está de saco cheio e acha todos ao seu redor inúteis.',
+      link: 'https://youtu.be/3-UdM086V_s'
+    }
+  ];
