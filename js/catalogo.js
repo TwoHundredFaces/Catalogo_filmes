3d473102482baef90faf6a325cf2422ccff8dc45
TwoHundredFaces <emersonsoares141@gmail.com>
2023-05-30 22:06:23 -0300

    versão final

--- js/catalogo.js
+++ js/catalogo.js
@@ -1,6 +1,7 @@
 let modeloJson = [
     {
       id: 1,
+      stts: 'Em cartaz',
       name: 'Me chame pelo seu nome',
       img: 'imgs/call.jpg',
       description: 'O jovem Elio está enfrentando outro verão preguiçoso na casa de seus pais na bela e lânguida paisagem italiana. Mas tudo muda com a chegada de Oliver, um acadêmico que veio ajudar a pesquisa de seu pai.',
@@ -8,6 +9,7 @@ let modeloJson = [
     },
     {
       id: 2,
+      stts: 'Em cartaz',
       name: 'Given',
       img: 'imgs/given.jpg',
       description: 'A história segue um grupo de quatro alunos de uma banda de rock amadora e as relações românticas que se formam entre eles: entre o guitarrista Ritsuka Uenoyama e o vocalista Mafuyu Satō (e seu cachorro, Kedama Satō), e entre o baixista Haruki Nakayama e o baterista Akihiko Kaji.',
@@ -15,6 +17,7 @@ let modeloJson = [
     },
     {
       id: 3,
+      stts: 'Em cartaz',
       name: 'Classmates',
       img: 'imgs/classmates.jpg',
       description: 'Conta a história de Kusakabe e Sajou, dois alunos que nunca se falaram antes, mas curiosamente passaram a se encontrar para treinar canto para uma apresentação na escola.',
@@ -22,6 +25,7 @@ let modeloJson = [
     },
     {
       id: 4,
+      stts: 'Em cartaz',
       name: 'Close',
       img: 'imgs/close.jpg',
       description: 'Léo e Rémi são dois garotos de treze anos que são melhores amigos. Eles passam as férias de verão juntos, compartilhando momentos muito próximos e inocentes. No entanto, na volta às aulas, os colegas começam a tecer comentários intrusivos sobre sua proximidade.',
@@ -29,6 +33,7 @@ let modeloJson = [
     },
     {
       id: 5,
+      stts: 'Em cartaz',
       name: 'Hoje eu quero voltar sozinho',
       img: 'imgs/hoje.jpg',
       description: 'Leonardo, um adolescente cego, tenta lidar com a mãe superprotetora ao mesmo tempo em que busca sua independência. Quando Gabriel chega em seu colégio, novos sentimentos começam a surgir em Leonardo, fazendo com que ele descubra mais sobre si mesmo e sua sexualidade.',
@@ -36,6 +41,7 @@ let modeloJson = [
     },
     {
       id: 6,
+      stts: 'Em cartaz',
       name: 'Yes, no or maybe',
       img: 'imgs/yesnomaybe.jpg',
       description: 'A série é estrelada por Kei Kunieda, um jovem e popular apresentador de televisão que mantém sua fachada de perfeição diante das câmeras, mas que em particular está de saco cheio e acha todos ao seu redor inúteis.',
--- js/script.js
+++ js/script.js
@@ -1,12 +1,65 @@
+var slides = document.querySelectorAll('.slide');
+var btns = document.querySelectorAll('.btn');
+let currentSlide = 1;
+
+    // Javascript for image slider manual navigation
+    var manualNav = function(manual){
+      slides.forEach((slide) => {
+        slide.classList.remove('active');
+
+        btns.forEach((btn) => {
+          btn.classList.remove('active');
+        });
+      });
+
+      slides[manual].classList.add('active');
+      btns[manual].classList.add('active');
+    }
+
+    btns.forEach((btn, i) => {
+      btn.addEventListener("click", () => {
+        manualNav(i);
+        currentSlide = i;
+      });
+    });
+
+    // Javascript for image slider autoplay navigation
+    var repeat = function(activeClass){
+      let active = document.getElementsByClassName('active');
+      let i = 1;
+
+      var repeater = () => {
+        setTimeout(function(){
+          [...active].forEach((activeSlide) => {
+            activeSlide.classList.remove('active');
+          });
+
+        slides[i].classList.add('active');
+        btns[i].classList.add('active');
+        i++;
+
+        if(slides.length == i){
+          i = 0;
+        }
+        if(i >= slides.length){
+          return;
+        }
+        repeater();
+      }, 10000);
+      }
+      repeater();
+    }
+    repeat();
+
+
 const c = (el) => document.querySelector(el);
 const cs = (el) => document.querySelectorAll(el);
-
+    
 modeloJson.map((item, index) => {
     let modeloItem = c('.models .modelo-item').cloneNode(true);
 
     modeloItem.querySelector('.modelo-item-img img').src = item.img;
     modeloItem.querySelector('.modelo-item-name').innerHTML = item.name;
-    modeloItem.querySelector('.modelo-item-desc').innerHTML = item.description;
     modeloItem.querySelector('.modelo-item a').href = item.link;
 
     c('.modelo-area').append(modeloItem);
